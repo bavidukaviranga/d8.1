fead869bcc290ff03ff2b1725c386b4a239b39d6
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-04-24 16:33:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("Hello");
+// console.log(10===10);
+
+//step 01
+// function getSum(x,y){
+//     return x+y;
+// }
+
+
+//step 02
+// let getSum=function (x,y){
+//     return x+y;
+// }
+
+//step03
+let getSum=function (x,y){
+        return x+y;
+     }
+let sum=getSum(20,10);
+console.log(sum);
